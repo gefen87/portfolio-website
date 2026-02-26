7dd9f225e56dbbb14e6b3b5d1ab58d70f652e9a2
Nadav Gefen <n.gefen87@gmail.com>
2026-02-26 12:58:13 +0500

    Add smart email link to HTML

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ window.onscroll = function() {
         btt.style.display = "none";
     }
 };
+// פונקציה חכמה לפתיחת אימייל בהתאם למכשיר
+function openEmail(event) {
+    event.preventDefault(); // מונע מהקישור לבצע את פעולת ברירת המחדל שלו
+    
+    // בדיקה האם המשתמש גולש ממכשיר נייד
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+    if (isMobile) {
+        // בנייד - פותח את אפליקציית המייל המובנית בצורה חלקה
+        window.location.href = "mailto:n.gefen87@gmail.com";
+    } else {
+        // במחשב - פותח לשונית חדשה של ג'ימייל
+        window.open("https://mail.google.com/mail/?view=cm&fs=1&to=n.gefen87@gmail.com", "_blank");
+    }
+}
